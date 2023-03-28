6e309878bf40a2ca19a9f2f92187b83c86fabcc6
Sinan <sinanfen@hotmail.com>
2023-03-28 23:54:13 +0300

    concrete movies service

---
+++ eTickets/Data/Services/Concrete/MoviesService.cs
@@ -0,0 +1,12 @@
+﻿using eTickets.Data.Base;
+using eTickets.Data.Services.Abstract;
+using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data.Services.Concrete
+{
+    public class MoviesService : EntityBaseRepository<Movie>, IMoviesService
+    {
+        public MoviesService(AppDbContext context) : base(context) { }
+    }
+}
