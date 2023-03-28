70f955f26fc7dafc5c8b8d01eaa11b0769eb9fef
Sinan <sinanfen@hotmail.com>
2023-03-28 23:53:34 +0300

    movieService

--- eTickets/Models/Movie.cs
+++ eTickets/Models/Movie.cs
@@ -1,10 +1,11 @@
-﻿using eTickets.Data.Enums;
+﻿using eTickets.Data.Base;
+using eTickets.Data.Enums;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace eTickets.Models
 {
-    public class Movie
+    public class Movie : IEntityBase
     {
         [Key]
         public int Id { get; set; }
