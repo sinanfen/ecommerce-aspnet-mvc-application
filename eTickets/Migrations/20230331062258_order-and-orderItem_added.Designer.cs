07810d3019b2b85cd7e2430f2a84b131824e4e3b
Sinan <sinanfen@hotmail.com>
2023-03-31 09:24:51 +0300

    order and orderitem added

--- eTickets/Migrations/20230331062258_order-and-orderItem_added.Designer.cs
+++ eTickets/Migrations/20230331062258_order-and-orderItem_added.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eTickets.Data;
 
@@ -11,9 +12,11 @@ using eTickets.Data;
 namespace eTickets.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230331062258_order-and-orderItem_added")]
+    partial class orderandorderItemadded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
