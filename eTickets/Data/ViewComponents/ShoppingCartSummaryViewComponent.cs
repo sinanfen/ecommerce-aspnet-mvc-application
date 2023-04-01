809bf176c46ea79d4388e974b6098b65ebaef654
Sinan <sinanfen@hotmail.com>
2023-04-01 09:11:20 +0300

    ShoppingCartSummary view component added

---
+++ eTickets/Data/ViewComponents/ShoppingCartSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using eTickets.Data.Cart;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Data.ViewComponents
+{
+    public class ShoppingCartSummaryViewComponent : ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartSummaryViewComponent(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+
+            return View(items.Count);
+        }
+    }
+}
