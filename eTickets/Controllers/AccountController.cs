b12ace7b9a612eae1570e261ccb12870dd0b3c6a
Sinan <sinanfen@hotmail.com>
2023-04-02 08:46:45 +0300

    created login view

---
+++ eTickets/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using eTickets.Data;
+using eTickets.Data.ViewModels;
+using eTickets.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly AppDbContext _context;
+
+        public AccountController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, AppDbContext context)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public IActionResult Login() => View(new LoginVM());
+
+
+    }
+}
+
