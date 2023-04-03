a41eb9aac6d17744c6c0aa241b6a40c66b384476
Sinan <sinanfen@hotmail.com>
2023-04-04 01:47:38 +0300

    movie search (filter) method refactored to search lowercase -

--- eTickets/Controllers/MoviesController.cs
+++ eTickets/Controllers/MoviesController.cs
@@ -38,12 +38,13 @@ namespace eTickets.Controllers
 
             if (!string.IsNullOrEmpty(searchString))
             {
-                //var filteredResult = allMovies.Where(n => n.Name.ToLower().Contains(searchString.ToLower()) || n.Description.ToLower().Contains(searchString.ToLower())).ToList();
+                var filteredResult = allMovies.Where(n => n.Name.ToLower().Contains(searchString.ToLower())
+                || n.Description.ToLower().Contains(searchString.ToLower())).ToList();
 
-                var filteredResultNew = allMovies.Where(n => string.Equals(n.Name, searchString, StringComparison.CurrentCultureIgnoreCase) 
-                || string.Equals(n.Description, searchString, StringComparison.CurrentCultureIgnoreCase)).ToList();
+                //var filteredResultNew = allMovies.Where(n => string.Equals(n.Name, searchString, StringComparison.CurrentCultureIgnoreCase) 
+                //|| string.Equals(n.Description, searchString, StringComparison.CurrentCultureIgnoreCase)).ToList();
 
-                return View("Index", filteredResultNew);
+                return View("Index", filteredResult);
             }
 
             return View("Index", allMovies);
