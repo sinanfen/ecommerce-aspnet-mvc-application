fd5fe76a2b8bfd847fd14c7ba698f146e28bca31
Sinan <sinanfen@hotmail.com>
2023-04-03 03:45:46 +0300

    login fixed

--- eTickets/Controllers/AccountController.cs
+++ eTickets/Controllers/AccountController.cs
@@ -74,6 +74,18 @@ namespace eTickets.Controllers
             }
             return View("RegisterCompleted");
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Movies");
+        }
+
+        public IActionResult AccessDenied(string ReturnUrl)
+        {
+            return View();
+        }
     }
 }
 
